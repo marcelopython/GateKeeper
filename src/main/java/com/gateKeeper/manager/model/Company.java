4fc645109e4a67d3e9bd700e5f307417d0119fb9
Marcelo <msr88957@gmail.com>
2024-07-13 11:27:58 -0300

    COnfiguração mysql

---
+++ src/main/java/com/gateKeeper/manager/model/Company.java
@@ -0,0 +1,135 @@
+package com.gateKeeper.manager.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+
+@Entity
+@Table(name = "companies")
+public class Company implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+   
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID uuid;
+    
+    private String name;
+    
+    private String email;
+    
+    private String document;
+    
+    private String phone;
+    
+    @CreationTimestamp
+    private Instant created_at;
+    
+    @UpdateTimestamp
+    private Instant updated_at;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Instant getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Instant created_at) {
+		this.created_at = created_at;
+	}
+
+	public Instant getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Instant updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    
+    
+    
+}
---
+++ src/main/java/com/gateKeeper/manager/model/User.java
@@ -0,0 +1,5 @@
+package com.gateKeeper.manager.model;
+
+public class User {
+
+}
