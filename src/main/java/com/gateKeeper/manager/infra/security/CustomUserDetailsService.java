be90ffc5ae821fc365d3f17a08c20f19bbb422fa
Marcelo <msr88957@gmail.com>
2024-07-13 23:10:25 -0300

    configuration authentication

---
+++ src/main/java/com/gateKeeper/manager/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.gateKeeper.manager.infra.security;
+
+import com.gateKeeper.manager.model.User;
+import com.gateKeeper.manager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+
+    }
+}
---
+++ src/main/java/com/gateKeeper/manager/infra/security/SecurityFilter.java
@@ -0,0 +1,68 @@
+package com.gateKeeper.manager.infra.security;
+
+import com.gateKeeper.manager.model.User;
+import com.gateKeeper.manager.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String login = tokenService.ValidateToken(
+                this.recoverToken(request)
+        );
+
+        if(login != null){
+
+            User user = userRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("User not found"));
+
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String recoverToken(HttpServletRequest request){
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            return null;
+        }
+
+        return authHeader.replace("Bearer ", "");
+
+    }
+
+
+}
